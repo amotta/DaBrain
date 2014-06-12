c7538b4a6a7ca49f305d4c17ba3b751c4db53e30
Alessandro Motta <amotta91@gmail.com>
2014-06-12 17:32:08 +0200

    Write log of firing neurons to file

--- src/dabrain.c
+++ src/dabrain.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "log.h"
 #include "net.h"
 
 void usage(){
@@ -19,16 +20,22 @@ int main(int argc, char ** argv){
 	};
 
 	netNew(&net);
-
 	netInit(&net);
 	printf("Net build\n");
 	printf("Size: %lu\n", net.numNeurons);
+
+	// prepare logging
+	FILE * firingFile = fopen("firing.log", "w");
 	
 	clock_t tic = clock();	
 	for(size_t i = 0; i < 1000; i++){
+		logFiring(&net, firingFile);
 		netUpdate(&net);
 	}
 	clock_t toc = clock();
+
+	// end logging
+	fclose(firingFile);
 
 	printf("Net updated\n");
 	printf("Time elapsed: %f\n", (double) (toc - tic) / CLOCKS_PER_SEC);	
