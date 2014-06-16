cb4472059ac51cbafaa1065aa9f3a5486275565a
Alessandro Motta <amotta91@gmail.com>
2014-06-16 16:19:02 +0200

    Adapt logging function for neuron firing to new architecture.

--- src/log.c
+++ src/log.c
@@ -3,9 +3,10 @@
 void logFiring(net_t * pNet, FILE * logFile){
 	if(!logFile) return;
 
-	for(size_t n = 0; n < pNet->numNeurons; n++){
-		if(pNet->neurons[n].dynState.firing){
-			fprintf(logFile, "%lu\t%lu\n", pNet->time, n);
+	for(int n = 0; n < pNet->numNeurons; n++){
+		float * state = &pNet->dynState[n * DYN_STATE_LEN];
+		if(state[DYN_STATE_FIRING]){
+			fprintf(logFile, "%d\t%d\n", pNet->t, n);
 		}
 	}
 }
