b87dde32f97d0c0d9a9427695c4ca9cd6da14bdd
Alessandro Motta <amotta91@gmail.com>
2014-06-16 19:15:36 +0200

    Logging of firing pattern in new architecture

--- src/log.c
+++ src/log.c
@@ -1,11 +1,10 @@
 #include "log.h"
 
-void logFiring(net_t * pNet, FILE * logFile){
+void logFiring(const net_t * pNet, FILE * logFile){
 	if(!logFile) return;
 
 	for(int n = 0; n < pNet->numNeurons; n++){
-		float * state = &pNet->dynState[n * DYN_STATE_LEN];
-		if(state[DYN_STATE_FIRING]){
+		if(pNet->firing[n]){
 			fprintf(logFile, "%d\t%d\n", pNet->t, n);
 		}
 	}
