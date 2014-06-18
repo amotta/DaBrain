88a9646097201056de957899d06a13fc953f02b7
Alessandro Motta <amotta91@gmail.com>
2014-06-18 16:34:07 +0200

    Clean up header file of IO module.

--- src/io.h
+++ src/io.h
@@ -1,12 +1,7 @@
 #ifndef IO_H
 #define IO_H
 
-int ioReadMat(
-	const char * fileName,
-	float * mat,
-	int rows,
-	int cols
-);
+int ioReadMat(const char * fileName, float * mat, int rows, int cols);
 int ioReadMatSize(const char * fileName, int * rows, int * cols);
 
 #endif
