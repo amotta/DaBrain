bc46457a924e2635418539dca6cee6e2e511d796
Alessandro Motta <amotta91@gmail.com>
2014-06-25 17:51:24 +0200

    Prepare for Hodgkin-Huxley model.

--- src/neuron.h
+++ src/neuron.h
@@ -2,20 +2,27 @@
 #define NEURON_H
 
 enum DYN_PARAM {
-	DYN_PARAM_A,
-	DYN_PARAM_B,
-	// voltage reset
-	DYN_PARAM_C,
-	// recovery reset
-	DYN_PARAM_D,
+	// membrane capacitance
+	DYN_PARAM_CM,
+	// maximum conductance for
+	// K channels
+	DYN_PARAM_GK,
+	// Na channels
+	DYN_PARAM_GNA,
+	// leakage
+	DYN_PARAM_GL,
 	DYN_PARAM_LEN
 };
 
 enum DYN_STATE {
 	// membrane voltage
 	DYN_STATE_V,
-	// recovery variable
-	DYN_STATE_U,
+	// K channel activation
+	DYN_STATE_N,
+	// Na channel activation
+	DYN_STATE_M,
+	// Na channel inactivation
+	DYN_STATE_H,
 	DYN_STATE_LEN
 };
 
